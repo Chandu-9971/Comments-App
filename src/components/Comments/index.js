447c5097929c28fdd4943be5469b8575604d9c99
Chandu Munagapati <chandhu.munagapati@gmail.com>
2022-12-13 07:09:06 +0000

    comments app

---
+++ src/components/Comments/index.js
@@ -0,0 +1,151 @@
+import {Component} from 'react'
+import v4 from 'uuid'
+import CommentItem from '../CommentItem'
+
+import './index.css'
+
+const initialContainerBackgroundClassNames = [
+  'amber',
+  'blue',
+  'orange',
+  'emerald',
+  'teal',
+  'red',
+  'light-blue',
+]
+
+class Comments extends Component {
+  state = {
+    nameInput: '',
+    commentInput: '',
+    commentsList: [],
+  }
+
+  deleteComment = commentId => {
+    const {commentsList} = this.state
+
+    this.setState({
+      commentsList: commentsList.filter(comment => comment.id !== commentId),
+    })
+  }
+
+  toggleIsLiked = id => {
+    this.setState(prevState => ({
+      commentsList: prevState.commentsList.map(eachComment => {
+        if (id === eachComment.id) {
+          return {...eachComment, isLiked: !eachComment.isLiked}
+        }
+        return eachComment
+      }),
+    }))
+  }
+
+  renderCommentsList = () => {
+    const {commentsList} = this.state
+
+    return commentsList.map(eachComment => (
+      <CommentItem
+        key={eachComment.id}
+        commentDetails={eachComment}
+        toggleIsLiked={this.toggleIsLiked}
+        deleteComment={this.deleteComment}
+      />
+    ))
+  }
+
+  onAddComment = event => {
+    event.preventDefault()
+
+    // To prevent the default behavior of form when submitted
+
+    const {nameInput, commentInput} = this.state
+
+    // Destructuring the username and comment from state variables
+
+    const initialBackgroundColorClassName = `initial-container ${
+      initialContainerBackgroundClassNames[
+        Math.ceil(
+          Math.random() * initialContainerBackgroundClassNames.length - 1,
+        )
+      ]
+    }` // initialContainerBackgroundClassNames[ random index ] will give the value at that corresponding index// Finally the value at the corresponding index will be assignned to the ﻿initialBackgroundColorClassName
+
+    // Math.ceil(Math.random() * initialContainerBackgroundClassNames.length - 1,) will return the random index
+    const newComment = {
+      id: v4(),
+      name: nameInput,
+      comment: commentInput,
+      date: new Date(),
+      isLiked: false,
+      initialClassName: initialBackgroundColorClassName,
+    }
+
+    // Forming an object using appropriate values to the keys of an object newComment.
+
+    this.setState(prevState => ({
+      commentsList: [...prevState.commentsList, newComment],
+      nameInput: '',
+      commentInput: '',
+    }))
+
+    // Using spread operator setting state variable commentLists
+  }
+
+  onChangeCommentInput = event => {
+    this.setState({
+      commentInput: event.target.value,
+    })
+  }
+
+  onChangeNameInput = event => {
+    this.setState({
+      nameInput: event.target.value,
+    })
+  }
+
+  render() {
+    const {nameInput, commentInput, commentsList} = this.state
+
+    return (
+      <div className="bg-container">
+        <div className="comments-container">
+          <h1 className="comment-heading">Comments</h1>
+          <div className="comments-inputs">
+            <form className="form" onSubmit={this.onAddComment}>
+              <p className="paragraph">Say something about 4.0 Technologies</p>
+              <input
+                type="text"
+                placeholder="Your Name"
+                className="name-input"
+                value={nameInput}
+                onChange={this.onChangeNameInput}
+              />
+              <textarea
+                className="comment-input"
+                placeholder="Your Comment"
+                rows="6"
+                onChange={this.onChangeCommentInput}
+                value={commentInput}
+              />
+              <button className="add-comment-button" type="submit">
+                Add Comment
+              </button>
+            </form>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
+              alt="comments"
+              className="image"
+            />
+          </div>
+          <hr className="line" />
+          <p className="heading">
+            <span className="comments-count">{commentsList.length}</span>
+            Comments
+          </p>
+          <ul className="comments-list">{this.renderCommentsList()}</ul>
+        </div>
+      </div>
+    )
+  }
+}
+export default Comments
